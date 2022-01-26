9f16920e0b9f52bcedb18aab0328ef0918b2accc
Ulric Esparra <ulric.esparra@oliveai.com>
2022-01-26 11:36:38 -0500

    Adding in component wrappers

--- src/index.ts
+++ src/index.ts
@@ -106,6 +106,62 @@ export default class Segment {
   }
 
   /**
+   * Set a component's handlers to a wrapped version that will call GA then the original handler
+   */
+  static wrap(component: whisper.Component) {
+    /**
+     * This generic function accepts the existing handler and the desired event function, returns a
+     * function that calls the event function and then calls the existing handler if it exists.
+     * I can't imagine there's not a cleaner way to write this but the principle is sound
+     */
+    function handlerWrapper<F extends (...args: unknown[]) => void>(
+      fn: F | undefined,
+      eventFn: () => void
+    ) {
+      return function _(...args: unknown[]) {
+        eventFn();
+        if (fn) fn(...args);
+      };
+    }
+
+    // TODO: Figure out what the type needs to be so we don't use any without making it a giant nest of conditional typing for every component handler
+    const onClickWrapper = <F extends (...args: any[]) => void>(
+      fn: F | undefined,
+      clickedComponent: whisper.Component
+    ) => {
+      return handlerWrapper(fn, () => Segment.trackComponentClicked(clickedComponent.type));
+    };
+
+    if ('onClick' in component) {
+      component.onClick = onClickWrapper(component.onClick, component);
+    }
+    if ('onLinkClick' in component) {
+      component.onLinkClick = onClickWrapper(component.onLinkClick, component);
+    }
+
+    return component;
+  }
+
+  /**
+   * Go through an array of components and their children and attempt to wrap handlers with event tracking
+   *
+   * TODO: Enhancements
+   * 1. Define a helpful options object: force wrap undefined handlers, override names, etc.
+   * 2. Extend the component type to some kind of TrackedComponent to help protect from accidentally wrapping components multiple times
+   */
+  static wrapComponents(components: whisper.Component[]) {
+    return components.map((component) => {
+      if ('children' in component) {
+        component.children = Segment.wrapComponents(
+          component.children
+        ) as typeof component.children;
+        return component;
+      }
+      return Segment.wrap(component);
+    });
+  }
+
+  /**
    * Sends the HTTP request to Segment based on given endpoint and body args
    *
    * Should not be called directly, use identify, page, or track instead
